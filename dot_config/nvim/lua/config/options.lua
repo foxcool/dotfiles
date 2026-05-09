921963947eb76d1708a6441f7fb48ab9d19478d8
Alexander Babenko <mail@darkfox.info>
2026-05-09 15:46:33 +0700

    add OSC 52 support in vim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,3 +9,18 @@ vim.opt.linebreak = true
 -- Line numbers (отключение относительной нумерации)
 vim.opt.relativenumber = false
 vim.opt.number = true
+
+-- Use OSC 52 clipboard over SSH (no system clipboard available on headless servers)
+if vim.env.SSH_TTY ~= nil then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
